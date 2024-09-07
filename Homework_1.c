db5cdbb7486358d8bb8efbbdfd116e63435c2f76
ansty <ansty@5CC1>
2024-09-07 20:48:30 +0300

    Добавьте файлы проекта.

---
+++ Homework_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main() {
+	puts("                                                                  ");
+	puts("               ________                                           ");
+	puts("              /        \\                                         ");
+	puts("             /          \\                                        ");
+	puts("            /            \\                                       ");
+	puts("           |--------------|                                       ");
+	puts("           |              |                                       ");
+	puts("           |         __   |                                       ");
+	puts("           |        | *|  |                                       ");
+	puts("           |________|__|__|                                       ");
+	puts("                                                                  ");
+	puts("                                                                  ");
+	puts("                                                                  ");
+	puts("                                                                  ");
+	puts("                                                                  ");
+	puts("                                                                  ");
+	puts("                                                                  ");
+}
